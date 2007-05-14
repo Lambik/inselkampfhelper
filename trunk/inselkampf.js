37002fb7836cbc1425c69b4658bbaf3af03ad001
lambik <lambik@4e6151ba-c92b-0410-a2a3-eb39d943d493>
2007-05-14 11:24:16 +0000

    The laboratory page contains a typo. It's fixed now!

--- trunk/inselkampf.js
+++ trunk/inselkampf.js
@@ -506,6 +506,11 @@ if( location.hostname.indexOf('213.203.194.123') != -1 ) {
 					
 				}
 				
+				// fix silly typo on the laboratory page
+				if (oElement.innerText.indexOf("the more") > -1) {
+					oElement.innerText = oElement.innerText.replace(/the more the more/, 'the more');
+				}
+				
 			}
 			
 			
